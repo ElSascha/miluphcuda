d6cfa21d4642928ad41625fdd2affe472eeec046
ElSascha <sascha.eckstein@student.uni-tuebingen.de>
2026-09-24 10:04:58 +0200

    edited scripts

--- spinning_cube_iron/run.sh
+++ spinning_cube_iron/run.sh
@@ -4,7 +4,7 @@
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=8
-#SBATCH --mem=64G
+#SBATCH --mem=16384
 #SBATCH --gres=gpu:1
 #SBATCH --time=20:00:00
 #SBATCH --output=%x.out   # Separates Log pro Job-Ausführung
